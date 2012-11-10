0bb033f6ab79c3070eba1a4870d8132f54b72371
James Toll <james@jtoll.com>
2012-11-10 14:46:22 -0600

    lsdir.r - significantly rework if/else branching

--- lsdir.r
+++ lsdir.r
@@ -1,39 +1,57 @@
 # Copyright © 2012 James Toll
+# normalizePath("..")
+# path.expand("~")
 
 lsdir <- function(path, names = "relative", all = FALSE, recursive = FALSE) {
+  # a function to list directories that actually works
   
+  # set a path if necessary
   if (missing(path)) {
     path <- "."
   }
+
+#   # isolate the first character of the path for testing
+#   pathID <- strsplit(path, "")[[1]][1]
+#   
+#   # test whether the path is absolute or relative
+#   if (pathID == "/" || pathID == "~"){
+#     absolute <- TRUE
+#   } else {
+#     absolute <- FALSE
+#   }
   
+  # recursion
   if (recursive == FALSE) {
     argRecursive <- "-maxdepth 1"
   } else if (recursive) {
     argRecursive <- ""
   }
-
+  
+  # piece together system command
   execFind <- paste("find", path, "-type d", argRecursive, "-mindepth 1 -print", sep = " ")
   
+  # execute system command 
   tmp <- system(execFind, intern = TRUE)
   
-  names <- match.arg(tolower(names), c("full", "relative", "base"))
-  
-  if (names == "relative" && all == TRUE) {
-    out <- tmp
-  } else if (names == "relative" && all == FALSE) {
-    out <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
-  } else if (names == "base" && all == TRUE) {
-    out <- basename(tmp)
-  } else if (names == "base" && all == FALSE) {
-    out <- basename(tmp)
-    out <- out[grep("^\\..*", out, invert = TRUE)]
-  } else if (names == "full" && all == TRUE) {
-    out <- paste(getwd(), gsub("\\./", "", tmp), sep = "/")
-  } else if (names == "full" && all == FALSE) {
-    out <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
-    out <- paste(getwd(), gsub("\\./", "", out), sep = "/")
+  # remove hidden files if all == FALSE
+  if (all == FALSE) {
+    tmp <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
   }
   
+  # match names argument
+  names <- match.arg(tolower(names), c("fullpath", "relative", "basename"))
+  
+  # format output based upon names argument
+  if (names == "basename") {
+    out <- basename(tmp)
+  } else if (names == "fullpath") {     #  && absolute == FALSE
+#    out <- paste(getwd(), gsub("\\./", "", tmp), sep = "/")
+    out <- normalizePath(tmp)
+  } else {
+    out <- tmp
+  }
+  
+  # clean up redundant "/" and return
   return(gsub("/+", "/", out))
   
 }
