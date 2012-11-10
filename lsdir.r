ec450c2b09b07033b455f3ceb82df5584579508a
James Toll <james@jtoll.com>
2012-11-10 14:43:52 -0600

    lsdir.r - fix recursion and pathnames

--- lsdir.r
+++ lsdir.r
@@ -1,24 +1,39 @@
 # Copyright © 2012 James Toll
 
-lsdir <- function(path, full.names = FALSE, recursive = FALSE, all.files = FALSE){
+lsdir <- function(path, names = "relative", all = FALSE, recursive = FALSE) {
   
   if (missing(path)) {
     path <- "."
   }
   
-  tmp <- list.dirs(path, full.names = full.names, recursive = recursive)
-  
-  if (full.names == FALSE && all.files == FALSE) {
-    out <- basename(tmp)
-    out <- out[grep("^\\..*", out, invert = TRUE)]
-  } else if (full.names == FALSE && all.files == TRUE) {
-    out <- basename(tmp)
-  } else if (full.names == TRUE && all.files == FALSE) {
-    out <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
-    out <- paste(getwd(), gsub("\\./", "", out), sep = "/")
-  } else if (full.names == TRUE && all.files == TRUE) {
-    out <- paste(getwd(), gsub("\\./", "", tmp), sep = "/")
+  if (recursive == FALSE) {
+    argRecursive <- "-maxdepth 1"
+  } else if (recursive) {
+    argRecursive <- ""
   }
 
+  execFind <- paste("find", path, "-type d", argRecursive, "-mindepth 1 -print", sep = " ")
+  
+  tmp <- system(execFind, intern = TRUE)
+  
+  names <- match.arg(tolower(names), c("full", "relative", "base"))
+  
+  if (names == "relative" && all == TRUE) {
+    out <- tmp
+  } else if (names == "relative" && all == FALSE) {
+    out <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
+  } else if (names == "base" && all == TRUE) {
+    out <- basename(tmp)
+  } else if (names == "base" && all == FALSE) {
+    out <- basename(tmp)
+    out <- out[grep("^\\..*", out, invert = TRUE)]
+  } else if (names == "full" && all == TRUE) {
+    out <- paste(getwd(), gsub("\\./", "", tmp), sep = "/")
+  } else if (names == "full" && all == FALSE) {
+    out <- tmp[grep("^\\..*", basename(tmp), invert = TRUE)]
+    out <- paste(getwd(), gsub("\\./", "", out), sep = "/")
+  }
+  
   return(gsub("/+", "/", out))
+  
 }
