c8d4e27f694d924c4dfcd6ddbb5e280bd95d9740
James Toll <james@jtoll.com>
2013-02-17 19:06:42 -0600

    move increment and timestamp to separate functions

--- noclobber.r
+++ noclobber.r
@@ -1,4 +1,4 @@
-# Copyright © 2012 James Toll
+# Copyright © 2012, 2013 James Toll
 
 noclobber <- function(filename, behavior = c("ask", "increment", "timestamp", "stop")) {
   # Try to prevent accidental overwriting of files 
@@ -10,40 +10,23 @@ noclobber <- function(filename, behavior = c("ask", "increment", "timestamp", "s
     if ((behavior == "ask")) {
       
       cat(paste("Warning: '", filename, "' already exists!\n", sep = ""))
-#       cat("Overwrite? [yes/no/increment]")
-#       answer <- match.arg(tolower(scan(n = 1, what = "character", quiet = TRUE)),
-#                           c("yes", "no", "increment"))
       answer <- match.arg(tolower(readline("Overwrite: [yes/no/increment/timestamp]? ")),
                           c("yes", "no", "increment", "timestamp"))
-      
       
       if (answer == "yes") {
         return(filename)
       } else if (answer == "no") {
-        stop("Nothing done!", call. = FALSE)
-      } else if (answer == "increment") {
-        noclobber(filename, behavior = "increment")
-      } else if (answer == "timestamp") {
-        noclobber(filename, behavior = "timestamp")
+        stop("File exists, nothing done!")
+      } else {
+        noclobber(filename, behavior = answer)
       }
 
     } else if ((behavior == "increment")) {
-      
-      part1 <- unlist(strsplit(filename, "\\.\\w{1,4}$"))
-      part2 <- sub(part1, "", filename)
-      
-      ext <- 1
-      
-      while (file.exists(paste(part1, "-", ext, part2, sep = ""))) {
-        ext <- ext + 1
-      }
-      
-      noclobber(paste(part1, "-", ext, part2, sep = ""), behavior = "increment")
-      
+      return(.fIncrement(filename))
     } else if ((behavior == "timestamp")) {
-      return(paste(format(Sys.time(), "%Y-%m-%dT%H-%M-%S"), filename, sep = "-"))
-    } else if (behavior == "stop") {
-      stop("File already exists!")
+      return(.fTimestamp(filename))
+    } else {
+      stop("File exists, nothing done!")
     } 
     
   } else {
@@ -51,4 +34,24 @@ noclobber <- function(filename, behavior = c("ask", "increment", "timestamp", "s
   }
 }
 
+.fIncrement <- function(filename) {
+  # while filename exists increment the filename
+  
+  part1 <- unlist(strsplit(filename, "\\.\\w{1,4}$"))
+  part2 <- sub(part1, "", filename)
+  
+  ext <- 1
+  
+  while (file.exists(paste(part1, "-", ext, part2, sep = ""))) {
+    ext <- ext + 1
+  }
+  
+  return(paste(part1, "-", ext, part2, sep = ""))
+}
+
+.fTimestamp <- function (filename) {
+  # add timestamp prefix to filename
+  # Mac OS X has issues with : (colons) in the time, therefore use - instead
+  return(paste(format(Sys.time(), "%Y-%m-%dT%H-%M-%S"), filename, sep = "-"))
+}
 
