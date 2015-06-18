8ecec2fa2579ceac3982fad502e418328b39c957
James Toll <james@jtoll.com>
2015-06-18 09:41:46 -0500

    Add a new behavior which automatically applies either a timestamp or increment. Create a new function parameter, form, which indicates whether you want to timestamp or increment.

--- noclobber.r
+++ noclobber.r
@@ -1,51 +1,55 @@
 # Copyright © 2012, 2013 James Toll
 
-noclobber <- function(filename, behavior = c("ask", "increment", "timestamp", "stop")) {
-  # Try to prevent accidental overwriting of files 
-  
-  if (file.exists(filename)) {
-  
-    behavior <- match.arg(tolower(behavior), c("ask", "increment", "timestamp", "stop"))
-    
-    if ((behavior == "ask")) {
-      
+noclobber <- function(filename, behavior = c("ask", "auto","stop"), form = c("timestamp", "increment")) {
+  # Try to prevent accidental overwriting of files
+
+  behavior <- match.arg(tolower(behavior), c("ask", "auto", "stop"))
+  form <- match.arg(tolower(form), c("timestamp", "increment"))
+
+  if ((behavior == "ask")) {
+
+    if (file.exists(filename)) {
       cat(paste("Warning: '", filename, "' already exists!\n", sep = ""))
       answer <- match.arg(tolower(readline("Overwrite: [yes/no/increment/timestamp]? ")),
                           c("yes", "no", "increment", "timestamp"))
-      
       if (answer == "yes") {
         return(filename)
       } else if (answer == "no") {
         stop("File exists, nothing done!")
       } else {
-        noclobber(filename, behavior = answer)
+        noclobber(filename, behavior = "auto", form = answer)
       }
-
-    } else if ((behavior == "increment")) {
-      return(.fIncrement(filename))
-    } else if ((behavior == "timestamp")) {
-      return(.fTimestamp(filename))
     } else {
+      return(filename)
+    }
+
+  } else if ((behavior == "auto")) {
+    if ((form == "timestamp")) {
+      return(.fTimestamp(filename))
+    } else if ((form == "increment")) {
+      return(.fIncrement(filename))
+    }
+  } else if ((behavior == "stop")) {
+    if (file.exists(filename)) {
       stop("File exists, nothing done!")
-    } 
-    
-  } else {
-    return(filename)
+    } else {
+      return(filename)
+    }
   }
 }
 
 .fIncrement <- function(filename) {
   # while filename exists increment the filename
-  
+
   part1 <- unlist(strsplit(filename, "\\.\\w{1,4}$"))
   part2 <- sub(part1, "", filename)
-  
+
   ext <- 1
-  
+
   while (file.exists(paste(part1, "-", ext, part2, sep = ""))) {
     ext <- ext + 1
   }
-  
+
   return(paste(part1, "-", ext, part2, sep = ""))
 }
 
