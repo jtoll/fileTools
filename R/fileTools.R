57b1a067e85668ad860f74563a28aa3b31068683
James Toll <james@jtoll.com>
2015-06-24 10:13:51 -0500

    Package Documentation
    Add a dummy function file which allows you to create a
    documentation file for the entire package. Rough in some
    basic information.

---
+++ R/fileTools.R
@@ -0,0 +1,32 @@
+#' fileTools: Miscellaneous functions created to facilitate the manipulation of files in R.
+#'
+#' This package was created just to make it a bit easier to use these miscellaneous
+#' functions. The package is still pretty rough, and the \code{\link{getGists}}
+#' function is broken since GitHub has updated their API.
+#'
+#' @section Functions:
+#' \itemize{
+#'  \item addRData
+#'  \item dirs
+#'  \item dirsr
+#'  \item files
+#'  \item getGists
+#'  \item lsdir
+#'  \item mkdir
+#'  \item noclobber
+#'  \item sad
+#'  \item timestamp
+#'  \item timestampr
+#'  \item webloc
+#'  }
+#'
+#' @section Copyright:
+#' Copyright © 2015 James C. Toll
+#'
+#' @section License:
+#' Distributed under the terms of the BSD 3-Clause License.
+#'
+#' @docType package
+#' @name fileTools
+NULL
+#> NULL
